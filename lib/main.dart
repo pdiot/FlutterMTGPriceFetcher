70a9dee0aecc0badb3bd37edb38bd820ee2e8086
Pierre Diot <diot.pierre1@gmail.com>
2022-05-15 01:04:30 +0200

    Recherche par nom

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: prefer_if_null_operators
+
 import 'dart:convert';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +14,13 @@ void main() {
 
 class CardModel {
   String name;
-  double eurPrice;
-  double eurFoilPrice;
+  String eurPrice;
+  String eurFoilPrice;
   String editionCode;
-  int collectorNumber;
+  String collectorNumber;
+  Set<String> pictures;
 
-  CardModel(this.name, this.eurPrice, this.eurFoilPrice, this.editionCode, this.collectorNumber);
+  CardModel(this.name, this.eurPrice, this.eurFoilPrice, this.editionCode, this.collectorNumber, this.pictures);
 }
 
 class MyApp extends StatelessWidget {
@@ -45,9 +49,10 @@ class CardDisplay extends StatefulWidget {
 class _CardDisplayState extends State<CardDisplay> {
   final _collectorNumberController = TextEditingController();
   final _extensionCodeController = TextEditingController();
-  final _picturesToDisplay = <String>{};
+  final _freeSearchController = TextEditingController();
   CardModel? _currentCard;
   final _savedCards = <CardModel>{};
+  final _cardsFromSearch = <CardModel>{};
 
   @override
   void dispose() {
@@ -74,7 +79,7 @@ class _CardDisplayState extends State<CardDisplay> {
                     enableInfiniteScroll: false,
                     autoPlay: false
                   ),
-                  items: _picturesToDisplay.map((uri) {
+                  items:_currentCard?.pictures.map((uri) {
                     return Builder(
                       builder: (BuildContext context) {
                         return Container(
@@ -93,8 +98,8 @@ class _CardDisplayState extends State<CardDisplay> {
                 padding: const EdgeInsets.all(10.0),
                 child: Row(
                   children: [
-                    Expanded(child: _currentCard?.eurPrice != null ? Text('Non foil : ${_currentCard?.eurPrice} €') : const Text('No non foil price found')),
-                    Expanded(child: _currentCard?.eurFoilPrice != null ? Text('Foil : ${_currentCard?.eurFoilPrice} €') : const Text('No foil price found')),
+                    Expanded(child: _currentCard?.eurPrice != "" ? Text('Non foil : ${_currentCard?.eurPrice} €') : const Text('No non foil price found')),
+                    Expanded(child: _currentCard?.eurFoilPrice != "" ? Text('Foil : ${_currentCard?.eurFoilPrice} €') : const Text('No foil price found')),
                   ],
                 ),
               )            
@@ -117,8 +122,27 @@ class _CardDisplayState extends State<CardDisplay> {
                 controller: _collectorNumberController,
               )
             ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          labelText: 'Enter card name'
+                        ),
+                        keyboardType: TextInputType.text,
+                        controller: _freeSearchController,
+                      )
+                    ),
+                    TextButton(onPressed: getCardFromNameField, child: const Text('Search')),
+                  ],
+                ),
+              ),
+            ),
             ButtonBar(children: [
-              TextButton(onPressed: getCard, child: const Text('Get card')),
+              TextButton(onPressed: getCardFromCodeCollector, child: const Text('Get card')),
               TextButton(onPressed: saveCard, child: const Text('Save card'))
             ],)
           ],
@@ -127,29 +151,74 @@ class _CardDisplayState extends State<CardDisplay> {
     );
   }
 
-  void getCard() async {
+  void getCardFromCodeCollector() async {
     final editionCode = _extensionCodeController.text.toString();
     final collectorNumber = _collectorNumberController.text.toString();
     final url = Uri.parse('https://api.scryfall.com/cards/${editionCode.toLowerCase()}/$collectorNumber');
     var response = await http.get(url);
     var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+
     setState(() {
+      var pictures = getPicturesFromJson(decodedResponse);
       _currentCard = CardModel(
         decodedResponse['name'],
-        double.parse(decodedResponse['prices']['eur']),
-        double.parse(decodedResponse['prices']['eur_foil']),
+        decodedResponse['prices']['eur'],
+        decodedResponse['prices']['eur_foil'],
         decodedResponse['set'],
-        int.parse(decodedResponse['collector_number']),
-      );
-      _picturesToDisplay.clear();
-      if (decodedResponse['card_faces'] != null) {
-        for (var face in (decodedResponse['card_faces'] as List<dynamic>)) { 
-          _picturesToDisplay.add(face['image_uris']['png']);
-        }
-      } else {
-        _picturesToDisplay.add(decodedResponse['image_uris']['png']);
-      }
+        decodedResponse['collector_number'],
+        pictures
+      );     
     });   
+  }
+
+  void getCardFromNameField() async {
+    final nameQuery = _freeSearchController.text.toString();
+    _freeSearchController.clear();
+    FocusScope.of(context).unfocus();
+    final uri = 'https://api.scryfall.com/cards/search?q=$nameQuery&unique=prints';
+    final encodedUri = Uri.encodeFull(uri);
+    var response = await http.get(Uri.parse(encodedUri));
+    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+    setState(() {
+      _cardsFromSearch.clear();
+      var resultList = decodedResponse['data'];
+      for (var cardJson in resultList) {
+        var pictures = getPicturesFromJson(cardJson);
+
+        var cardModel = CardModel(
+          cardJson['name'], 
+          cardJson['prices']['eur'] == null ? "" : cardJson['prices']['eur'],
+          cardJson['prices']['eur_foil'] == null ? "" : cardJson['prices']['eur_foil'],
+          cardJson['set'],
+          cardJson['collector_number'],
+          pictures
+        );
+        _cardsFromSearch.add(cardModel);
+      }
+      if (_cardsFromSearch.isNotEmpty) {
+        pushCardListFromSearch();
+      } else {
+        Fluttertoast.showToast(msg: 'No Results');
+      }
+    }); 
+  }
+
+  Set<String> getPicturesFromJson(cardJson) {
+    var pictures = <String>{};
+    if (cardJson['card_faces'] != null) {
+      for (var face in (cardJson['card_faces'] as List<dynamic>)) {
+        if (face['image_uris'] != null) {
+          pictures.add(face['image_uris']['png']);
+        } else {
+          if (cardJson['image_uris'] != null) {
+            pictures.add(cardJson['image_uris']['png']);
+          }
+        }
+      }
+    } else {
+      pictures.add(cardJson['image_uris']['png']);
+    }
+    return pictures;
   }
 
   void saveCard() {
@@ -170,7 +239,7 @@ class _CardDisplayState extends State<CardDisplay> {
           child: Row(
             children: [
               Expanded(child: Text(savedCard.name), flex: 3,),
-              Expanded(child: Text(savedCard.editionCode), flex: 1,),
+              Expanded(child: Text(savedCard.editionCode.toUpperCase()), flex: 1,),
               Expanded(child: Text(savedCard.collectorNumber.toString()), flex: 1,),
               Expanded(child: Text(savedCard.eurPrice.toString()), flex: 1,),
               Expanded(child: Text(savedCard.eurFoilPrice.toString()), flex: 1,),
@@ -188,5 +257,50 @@ class _CardDisplayState extends State<CardDisplay> {
         body: ListView(children: widgetList),
       );
     }));
+  }
+
+  void pushCardListFromSearch() {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (context) {
+        final cards = _cardsFromSearch.map((cardFromSearchResults) {
+          return Card(
+            child: Row(
+              children: [
+                Expanded(child: Text(cardFromSearchResults.name), flex: 3,),
+                Expanded(child: Text(cardFromSearchResults.editionCode), flex: 1,),
+                Expanded(child: Text(cardFromSearchResults.collectorNumber.toString()), flex: 1,),
+                Expanded(child: Text(cardFromSearchResults.eurPrice.toString()), flex: 1,),
+                Expanded(child: Text(cardFromSearchResults.eurFoilPrice.toString()), flex: 1,),
+                Expanded(child: TextButton(
+                  child: const Text('OK'), 
+                  onPressed: () {
+                    setState(() {
+                      _currentCard = cardFromSearchResults;
+                      Navigator.of(context).pop();
+                    });
+                  },
+                ),)
+              ]
+            ),
+          );
+        });
+        final widgetList = cards.isNotEmpty ? cards.toList() : <Widget>[];
+      return Scaffold(
+        appBar: AppBar(title: const Text('Search Results'),
+          actions: [
+            IconButton(
+              onPressed: () {
+              _cardsFromSearch.clear();
+              Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.backspace_outlined), 
+              tooltip: 'Clean list and go back',
+            )
+          ],
+        ),
+        body: ListView(children: widgetList),
+      );
+      })
+    );
   }
 }
